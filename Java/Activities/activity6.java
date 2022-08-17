f29411501cc4febc9314853cc277f09ab4221427
Avishek Chakraborty <111413198+avishekchakraborty2019@users.noreply.github.com>
2022-08-17 20:14:26 +0530

    Add files via upload

---
+++ Java/Activities/activity6.java
@@ -0,0 +1,65 @@
+package Activities;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class activity6 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		//There is a plane with max 10 passengers
+        Plane plane = new Plane(10);
+        //Add passengers on the list
+        plane.onboard("John");
+        plane.onboard("Steve");
+        plane.onboard("Anna");
+        //Plane takes off
+        System.out.println("Plane took off at: " + plane.takeOff());
+        //Print list of people on board
+        System.out.println("People on the plane: " + plane.getPassengers());
+        //Flying.....
+        Thread.sleep(5000);
+        //Plane has landed
+        plane.land();
+        //Plane lands
+        System.out.println("Plane landed at: " + plane.getLastTimeLanded());
+        System.out.println("People on the plane after landing: " + plane.getPassengers());
+	}
+
+}
+class Plane {
+    private List<String> passengers;
+    private int maxPassengers;
+    private Date lastTimeTookOf;
+    private Date lastTimeLanded;
+
+    public Plane(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+        this.passengers = new ArrayList<>();
+    }
+
+    public void onboard(String passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public Date takeOff() {
+        this.lastTimeTookOf = new Date(maxPassengers);
+        return lastTimeTookOf;
+    }
+
+    public void land() {
+        this.lastTimeLanded = new Date(maxPassengers);
+        this.passengers.clear();
+    }
+
+    public Date getLastTimeLanded() {
+        return lastTimeLanded;
+    }
+
+    public List<String> getPassengers() {
+        return passengers;
+    }
+}
+
+
