11acc0f1f3ce93670a8c06efde3f1b408b4dd5c3
avishekchakraborty2019 <111413198+avishekchakraborty2019@users.noreply.github.com>
2022-08-17 11:31:36 +0530

    Add files via upload
    Activity 1

---
+++ Java/Activities/activity1.java
@@ -0,0 +1,18 @@
+package Activities;
+
+public class activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "Black";
+        toyota.transmission = "Manual";
+    
+        //Using Car class method
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+	}
+}
+
